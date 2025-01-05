a031b810bf365340efa4c9dada3a802d01d002db
yonsmd <130866560+yonsmd@users.noreply.github.com>
2025-01-05 21:31:10 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
+
+
 function sendToWhatsapp(){
+    
     let number = "+9647505887492"
     let name = document.getElementById('name').value
     let email = document.getElementById('email').value
     let message = document.getElementById('message').value
+    let y = document.getElementById('yes').checked
+    let n = document.getElementById('no').checked
+    if(y==true && n==false){
+        var check = "Yes"
+    }else{
+        check = "No"
+    }
 
-    var url = "https://wa.me/" + number + "?text="+"Name: "+name+"%0a"+"Email: "+email+"%0a"+"Message: "+message+"%0a%0a"
+    var url = "https://wa.me/" + number + "?text="+"Name: "+name+"%0a"+"Email: "+email+"%0a"+"Check: "+check+"%0a"+"Message: "+message+"%0a%0a"
     window.open(url,"_blank").focus()
+
 }
